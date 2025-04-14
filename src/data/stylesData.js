622d407b6e27bd3addaa9a768ab39e1300846252
Melvin <melvinmps11301@gmail.com>
2025-04-15 00:13:59 +0300

    updated style data

--- src/data/stylesData.js
+++ src/data/stylesData.js
@@ -2,21 +2,71 @@ const styles = {
   hair: {
     MC: {
       name: "Modern Classics",
+      type: [
+        { name: "Modern Mullet", images: [] },
+        { name: "Pompadur Fade", images: [] },
+        { name: "Side Part", images: [] },
+      ],
     },
     PAC: {
       name: "Professional & Clean",
+      type: [
+        { name: "Low Taper Fade", images: [] },
+        { name: "Crew Cut", images: [] },
+        { name: "Comb Over", images: [] },
+      ],
     },
     BAE: {
       name: "Bold & Expressive",
+      type: [
+        { name: "Mohawk Fade", images: [] },
+        { name: "Buzz Cut with Design", images: [] },
+        { name: "Textured Undercut", images: [] },
+      ],
     },
     NAW: {
       name: "Natural & Wavy",
+      type: [
+        { name: "Mid Length Waves", images: [] },
+        { name: "Curly High Top Fade", images: [] },
+        { name: "Natural Waves and Curls", images: [] },
+      ],
     },
     YAT: {
       name: "Youthful & Trendy",
+      type: [
+        { name: "Brocolli Cut", images: [] },
+        { name: "Edgar Cut", images: [] },
+        { name: "Textured Crop", images: [] },
+      ],
     },
   },
-  beard: {},
+  beard: {
+    BSP: {
+      name: "Bold & Statement Beards",
+      type: [
+        { name: "Tapered Beard", images: [] },
+        { name: "Beardstache", images: [] },
+        { name: "Wild Beard", images: [] },
+      ],
+    },
+    CPC: {
+      name: "Clean & Professional",
+      type: [
+        { name: "Stubble Beard", images: [] },
+        { name: "Circle Beard", images: [] },
+        { name: "Classic Full Beard", images: [] },
+      ],
+    },
+    CAU: {
+      name: "Creative & Unique",
+      type: [
+        { name: "Patchy Anchored Stache", images: [] },
+        { name: "Freestyle Beard", images: [] },
+        { name: "Imperial Moustache", images: [] },
+      ],
+    },
+  },
 };
 
 export default styles;
