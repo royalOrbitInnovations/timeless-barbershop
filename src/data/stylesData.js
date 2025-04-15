032b2f1a41a96d2f04db0ae99d8a159ed0391ed0
Melvin <melvinmps11301@gmail.com>
2025-04-15 14:34:33 +0300

    updated hair styles

--- src/data/stylesData.js
+++ src/data/stylesData.js
@@ -35,25 +35,25 @@ const styles = {
         {
           name: "Low Taper Fade",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/lowtaperfade-1.webp",
+            "/images/haircuts/lowtaperfade-2.webp",
+            "/images/haircuts/lowtaperfade-3.webp",
           ],
         },
         {
           name: "Crew Cut",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/crewcut-1.webp",
+            "/images/haircuts/crewcut-2.webp",
+            "/images/haircuts/crewcut-3.webp",
           ],
         },
         {
           name: "Comb Over",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/combover-1.webp",
+            "/images/haircuts/combover-2.webp",
+            "/images/haircuts/combover-3.webp",
           ],
         },
       ],
@@ -64,25 +64,25 @@ const styles = {
         {
           name: "Mohawk Fade",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/mohawkfade-1.webp",
+            "/images/haircuts/mohawkfade-2.webp",
+            "/images/haircuts/mohawkfade-3.webp",
           ],
         },
         {
           name: "Buzz Cut with Design",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/buzzcutwithdesign-1.webp",
+            "/images/haircuts/buzzcutwithdesign-2.webp",
+            "/images/haircuts/buzzcutwithdesign-3.webp",
           ],
         },
         {
           name: "Textured Undercut",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/texturedundercut-1.webp",
+            "/images/haircuts/texturedundercut-2.webp",
+            "/images/haircuts/texturedundercut-3.webp",
           ],
         },
       ],
@@ -93,17 +93,17 @@ const styles = {
         {
           name: "Mid Length Waves",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/midlengthwavy-1.webp",
+            "/images/haircuts/midlengthwavy-2.webp",
+            "/images/haircuts/midlengthwavy-3.webp",
           ],
         },
         {
           name: "Curly High Top Fade",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/curlyhightopfade-1.webp",
+            "/images/haircuts/curlyhightopfade-2.webp",
+            "/images/haircuts/curlyhightopfade-3.webp",
           ],
         },
       ],
@@ -114,17 +114,17 @@ const styles = {
         {
           name: "Brocolli Cut",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/broccolicut-1.webp",
+            "/images/haircuts/broccolicut-2.webp",
+            "/images/haircuts/broccolicut-3.webp",
           ],
         },
         {
           name: "Edgar Cut",
           images: [
-            "/images/haircuts/mullet-1.webp",
-            "/images/haircuts/mullet-2.webp",
-            "/images/haircuts/mullet-3.webp",
+            "/images/haircuts/edgarcut-1.webp",
+            "/images/haircuts/edgarcut-2.webp",
+            "/images/haircuts/edgarcut-3.webp",
           ],
         },
       ],
