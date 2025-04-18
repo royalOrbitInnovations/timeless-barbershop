4707512ab3f9022453434f9e6bebf1be0e3145ec
Melvin <melvinmps11301@gmail.com>
2025-04-18 14:58:05 +0300

    updated book a chair button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Button({ text }) {
   return (
-    <Link href="tel:55360324">
-      <button className="border border-gold text-white px-[5rem] py-[2rem] text-[2.5rem] rounded-full bg-gold hover:bg-black hover:cursor-pointer transition-all duration-300 ease-in-out hover:scale-105 max-5xl:text-[2rem] shadow-2xl">
-        {text}
-      </button>
-    </Link>
+    <button
+      onClick={() => (window.location.href = "tel:55360324")}
+      className="border border-gold text-white px-[5rem] py-[2rem] text-[2.5rem] rounded-full bg-gold hover:bg-black hover:cursor-pointer transition-all duration-300 ease-in-out hover:scale-105 max-5xl:text-[2rem] shadow-2xl"
+    >
+      {text}
+    </button>
   );
 }
